e6dce00b5d34a5e598ca498c6e1fb2fea9395095
YarosShemet <122221404+YarosShemet@users.noreply.github.com>
2023-01-08 17:29:55 +0100

    Rename Yaraslau_Shemet_108887_pd2.R to IRIS_classification.R

--- IRIS_classification.R
+++ IRIS_classification.R
@@ -1,28 +1,28 @@
 library(ks)
 df <- as.data.frame(iris)
 
-#Dla stworzenia wykresu potrzebuje tylko dw�ch zmiennych regresor�W 
+#Dla stworzenia wykresu potrzebuje tylko dwóch zmiennych regresoróW 
 
 #1
 Zestaw_sepal_X <- data.frame(df$Sepal.Length, df$Sepal.Width)
 Zestaw_petal_X <- data.frame(df$Petal.Length, df$Petal.Width)
 Y <- data.frame(df$Species)
 
-H1 <- Hscv(x=Zestaw_sepal_X) #optymalna warto�� estymatora macierzy bandwidth wg sprawdzianu krzyzowego
+H1 <- Hscv(x=Zestaw_sepal_X) #optymalna wartoœæ estymatora macierzy bandwidth wg sprawdzianu krzyzowego
 H2 <- Hscv(x=Zestaw_petal_X)
 
 kde_sepal <- ks::kde(x = Zestaw_sepal_X, H = H1)
 image(kde_sepal$eval.points[[1]], kde_sepal$eval.points[[2]], kde_sepal$estimate,
-      col = viridis::viridis(20), xlab = "D�ugo�� listk�w", ylab = "Szeroko�� listk�w")
+      col = viridis::viridis(20), xlab = "D³ugoœæ listków", ylab = "Szerokoœæ listków")
 points(kde_sepal$x) 
 
 kde_petal <- ks::kde(x = Zestaw_petal_X, H = H2)
 image(kde_petal$eval.points[[1]], kde_petal$eval.points[[2]], kde_petal$estimate,
-      col = viridis::viridis(20), xlab = "D�ugo�� p�atk�w", ylab = "Szeroko�� p�atk�w")
+      col = viridis::viridis(20), xlab = "D³ugoœæ p³atków", ylab = "Szerokoœæ p³atków")
 points(kde_petal$x) 
 
-plot(kde_sepal, display = "persp", col.fun = viridis::viridis, xlab = "D�ugo�� listków", ylab = "Szeroko�� listk�w")
-plot(kde_petal, display = "persp", col.fun = viridis::viridis, xlab = "D�ugo�� p�atk�w", ylab = "Szeroko�� p�atk�w")
+plot(kde_sepal, display = "persp", col.fun = viridis::viridis, xlab = "D³ugoœæ listkÃ³w", ylab = "Szerokoœæ listków")
+plot(kde_petal, display = "persp", col.fun = viridis::viridis, xlab = "D³ugoœæ p³atków", ylab = "Szerokoœæ p³atków")
 
 #2
 train.size <- 0.8 * nrow(df)
@@ -45,10 +45,10 @@ library(class)
 prognoza5 <- knn(x_train,x_test,cl=y_train,k=5)
 prognoza50 <- knn(x_train,x_test,cl=y_train,k=50)
 
-#tabela trafno�ci
+#tabela trafnoœci
 tt <- table(prognoza5,y_test)
 tt
 
-#obliczamy dok�adno�� klasyfikacji dla zbioru testowego
+#obliczamy dok³adnoœæ klasyfikacji dla zbioru testowego
 dokladnosc <- function(x){sum(diag(x)/(sum(rowSums(x)))) * 100}
 dokladnosc(tt)
